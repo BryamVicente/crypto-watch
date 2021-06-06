a717e00d688fa009b71847957fa6e1e32d20b05b
github username <vicenteBryam@gmail.com>
2021-06-05 23:59:13 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const myToken = config.MY_API_TOKEN
+
 fetch("https://bravenewcoin.p.rapidapi.com/asset?type=CRYPTO&status=ACTIVE", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "afc5e9bcfdmshea1cbc205c5a1b7p1e02ebjsnd042a705e062",
+		"x-rapidapi-key": `${myToken}`,
 		"x-rapidapi-host": "bravenewcoin.p.rapidapi.com"
 	}
 })
